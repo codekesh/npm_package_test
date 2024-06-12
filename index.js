8bf4eba7a84e25d45c643e4ecfe290dfe8ddabe8
Keshav Tulsyan (External) <x-keshav.tulsyan@modelon.com>
2024-06-12 14:43:55 +0530

    for mat changes

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // this is calculator for 2 variables
 function calculator(a, b, c) {
-    if (c === "+") {
-      return `Result will be ${a + b}`;
-    } else if (c === "-") {
-      return `Result will be ${a - b}`;
-    } else if (c === "*") {
-      return `Result will be ${a * b}`;
-    } else if (c === "/") {
-      return `Result will be ${a / b}`;
-    } else if (c === ">") {
-      return a > b ? "a is greater than b" : "a is not greater than b";
-    } else if (c === "<") {
-      return a < b ? "a is lesser than b" : "a is not lesser than b";
-    } else if (c === "=") {
-      return a === b ? "a is equal to b" : "a is not equal to b";
-    } else {
-      return "Invalid operation";
-    }
+  if (c === "+") {
+    return `Result will be ${a + b}`;
+  } else if (c === "-") {
+    return `Result will be ${a - b}`;
+  } else if (c === "*") {
+    return `Result will be ${a * b}`;
+  } else if (c === "/") {
+    return `Result will be ${a / b}`;
+  } else if (c === ">") {
+    return a > b ? "a is greater than b" : "a is not greater than b";
+  } else if (c === "<") {
+    return a < b ? "a is lesser than b" : "a is not lesser than b";
+  } else if (c === "=") {
+    return a === b ? "a is equal to b" : "a is not equal to b";
+  } else {
+    return "Invalid operation";
   }
-  module.exports = calculator;
-  
+}
+module.exports = calculator;
